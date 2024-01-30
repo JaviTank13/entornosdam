b9d5be7bfa4666126899e66893b95dfc9fc675ea
daw23-14 <daw23-14@c16x02.iescierva.net>
2024-01-30 09:13:02 +0100

    ED1 añadido manin

---
+++ Ejemplo1.java
@@ -0,0 +1,9 @@
+public class Ejemplo1 {
+    public static void main(String[] args) {
+        System.out.println("Mi home' Manin destructor de Alcayatas");  
+    }
+}
+
+// git add .
+//got commmit -am "codigotarea comentario"
+//git push origin nombreRama
